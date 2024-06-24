8719423b019796f96fb8914e183594db48c6b923
oneieo <sun020716@gmail.com>
2024-06-24 22:18:42 +0900

    style: footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,14 @@
 const Footer = () => {
-  return <div>푸터입니다</div>;
+  return (
+    <div className="w-full h-32 bg-slate-200 relative translate-y-0 flex flex-col ">
+      <div className="ml-64 mt-7">
+        <img src="/public/showing.png" className="w-28" />
+        <p>
+          (재)예술경영지원센터 공연예술통합전산망 OpenAPI 이용, www.kopis.or.kr
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
