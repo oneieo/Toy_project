5c1a46d0ef578a8b5fdfd50e39a7a62c00cff07c
oneieo <sun020716@gmail.com>
2024-06-25 00:16:56 +0900

    style: main

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -3,8 +3,12 @@ const NavBar = () => {
     <div className="w-full h-12 flex items-center justify-between">
       <img src="/public/showing.png" className="w-28 ml-64" />
       <div className="mr-64">
-        <button className="border-none bg-transparent text-base">Home</button>
-        <button className="border-none bg-transparent text-base">Nearby</button>
+        <button className="border-none bg-transparent text-base cursor-pointer mr-9">
+          Home
+        </button>
+        <button className="border-none bg-transparent text-base cursor-pointer">
+          Nearby
+        </button>
       </div>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -25,8 +25,8 @@ const Main = () => {
   // map으로 뿌려야하나 ??
 
   return (
-    <div className="w-full h-full bg-gray-100">
-      <h1 className="w-96 ml-80 mt-28 text-4xl font-semibold text-gray-900">
+    <div className="w-full h-full">
+      <h1 className="w-4/5 ml-80 mt-28 text-5xl font-semibold text-gray-900 leading-tight">
         공연장 정보를 확인하는 <br />
         가장 좋은 방법 !
       </h1>
@@ -36,7 +36,7 @@ const Main = () => {
             return (
               <div
                 key={item.categoryName}
-                className="flex flex-col items-center"
+                className="flex flex-col items-center cursor-pointer font-semibold text-gray-700"
               >
                 <img src={item.icon} className="w-24" />
                 <p className="text-xl">{item.categoryName}</p>
@@ -45,8 +45,38 @@ const Main = () => {
           })}
         </div>
         <div className="ml-80 font-semibold">
-          <p>내 주변 공연장을 확인하고 싶다면?</p>
-          <p className="text-sky-500">가까운 공연장 찾기</p>
+          <p className="text-lg">내 주변 공연장을 확인하고 싶다면?</p>
+          <p className="text-sky-500 cursor-pointer">가까운 공연장 찾기</p>
+        </div>
+      </div>
+      <div>
+        <div className="flex ml-80 mt-20 items-end">
+          <h2>공연중</h2>
+          <p className="ml-8 text-lg font-semibold text-gray-400">
+            다양한 장르의 공연들
+          </p>
+        </div>
+        <div className="flex ml-80 gap-[30px]">
+          <img
+            src=""
+            className="w-[400px] h-[500px] bg-slate-300 border-none rounded-[20px]"
+          />
+          <img
+            src=""
+            className="w-[400px] h-[500px] bg-slate-300 border-none rounded-[20px]"
+          />
+          <img
+            src=""
+            className="w-[400px] h-[500px] bg-slate-300 border-none rounded-[20px]"
+          />
+          <img
+            src=""
+            className="w-[400px] h-[500px] bg-slate-300 border-none rounded-[20px]"
+          />
+          <img
+            src=""
+            className="w-[400px] h-[500px] bg-slate-300 border-none rounded-[20px]"
+          />
         </div>
       </div>
     </div>
